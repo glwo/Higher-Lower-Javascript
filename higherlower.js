4ced8ee483b53adb50afc8a906b95bc0dcf3600e
glen <glenwojnar@yahoo.com>
2023-05-24 17:37:21 -0400

    functionality working

--- higherlower.js
+++ higherlower.js
@@ -1,21 +1,56 @@
-let num = Math.floor(Math.random() * 20) + 1;
+let maxNumber = 0;
+let num = 0;
+let guesses = [];
 
-function do_guess(){
+while (maxNumber <= 0 || isNaN(maxNumber)) {
+    let input = prompt("Enter the maximum number for the game:");
+
+    maxNumber = Math.round(Number(input));
+}
+
+num = Math.floor(Math.random() * maxNumber) + 1;
+
+document.getElementById("guessANum").innerHTML = `Guess a number between 1 and ${maxNumber}`;
+
+function haveGuess(guess) {
+    return guesses.includes(guess);
+}
+
+function doGuess() {
     let guess = Number(document.getElementById("guess").value)
 
     let message = document.getElementById("message");
 
-    if (guess == num) {
-        message.innerHTML = "You got it!"
+    // if (!guess) {
+    //     message.innerHTML = "Please enter a guess!"
+    // }
+
+    if (isNaN(guess)) {
+        message.innerHTML = "That is not a number!";
+    }
+
+    else if (guess < 1 || guess > maxNumber) {
+        message.innerHTML = "That number is not in range, try again.";
+    }
+
+    else if (haveGuess(guess)) {
+        message.innerHTML = "You already guessed that number!";
+    }
+
+    else if (guess == num) {
+        guesses.push(guess);
+        message.innerHTML = `You got it! It took you ${guesses.length} tries and your guesses were ${guesses.join(", ")}.`;
     }
 
     else if (guess > num) {
-        message.innerHTML = "No, try a lower number."
+        guesses.push(guess);
+        message.innerHTML = "No, try a lower number.";
     }
 
     else {
-        message.innerHTML = "No, try a higher number."
+        guesses.push(guess);
+        message.innerHTML = "No, try a higher number.";
     }
 }
 
-document.getElementById("guessButton").addEventListener("click", do_guess);
+document.getElementById("guessButton").addEventListener("click", doGuess);
